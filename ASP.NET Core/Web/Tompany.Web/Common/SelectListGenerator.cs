e3d46b812a49c50b0954558a7ef85a808adbf63c
mvarbanov97 <momchil.varbanov97@gmail.com>
2020-04-28 09:25:55 +0300

    introduced SelectListGenerator

---
+++ ASP.NET Core/Web/Tompany.Web/Common/SelectListGenerator.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tompany.Services.Data.Contracts;
+using Tompany.Web.ViewModels.Destinations;
+
+namespace Tompany.Web.Common
+{
+    public static class SelectListGenerator
+    {
+        public static IEnumerable<SelectListItem> GetAllDestinations(IDestinationService destinationsService)
+        {
+            var destinations = destinationsService.GetAllDestinationsAsync().GetAwaiter().GetResult();
+            var groups = new List<SelectListGroup>();
+            foreach (var destinationViewModel in destinations)
+            {
+                if (groups.All(g => g.Name != destinationViewModel.Name))
+                {
+                    groups.Add(new SelectListGroup { Name = destinationViewModel.Name });
+                }
+            }
+
+            return destinations.Select(x => new SelectListItem
+            {
+                Value = x.Id.ToString(),
+                Text = x.Name,
+            });
+        }
+    }
+}
