b309dfe0cac947099fea96687afa1a44b2f18cac
mvarbanov97 <momchil.varbanov97@gmail.com>
2021-01-11 21:19:48 +0200

    Added migrations for chat entities

---
+++ ASP.NET Core/Data/Tompany.Data/Migrations/20201201141504_AddEfDeletableEntityColumnsToChatEntitites.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Tompany.Data.Migrations
+{
+    public partial class AddEfDeletableEntityColumnsToChatEntitites : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedOn",
+                table: "Groups",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Groups",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedOn",
+                table: "ChatMessages",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "ChatMessages",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedOn",
+                table: "Groups");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Groups");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedOn",
+                table: "ChatMessages");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "ChatMessages");
+        }
+    }
+}
