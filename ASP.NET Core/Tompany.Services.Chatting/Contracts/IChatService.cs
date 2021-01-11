8a08a271fca4fc3fbdd62ded0a157f937f534407
mvarbanov97 <momchil.varbanov97@gmail.com>
2021-01-12 00:18:35 +0200

    Add ChatHub and ChatService and connect the server-side code to the client methods

---
+++ ASP.NET Core/Tompany.Services.Chatting/Contracts/IChatService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tompany.Services.Chatting
+{
+    public interface IChatService
+    {
+        Task AddUserToGroup(string groupName, string toUsername, string fromName);
+
+        Task<string> SendMessageToUser(string fromUsername, string toUsername, string message, string group);
+
+        Task ReceiveNewMessage(string fromUsername, string message, string group);
+    }
+}
