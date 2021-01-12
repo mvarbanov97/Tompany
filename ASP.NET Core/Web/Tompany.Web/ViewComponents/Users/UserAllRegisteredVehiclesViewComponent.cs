f5fb827722a03bbf638a0f57ab7f3b1066f00b39
mvarbanov97 <momchil.varbanov97@gmail.com>
2021-01-12 03:13:21 +0200

    Implement Profile ViewComponents

---
+++ ASP.NET Core/Web/Tompany.Web/ViewComponents/Users/UserAllRegisteredVehiclesViewComponent.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tompany.Data.Models;
+using Tompany.Services.Data.Contracts;
+using Tompany.Web.ViewModels.Cars.ViewModels;
+using Tompany.Web.ViewModels.Users.ViewModels;
+
+namespace Tompany.Web.ViewComponents.Users
+{
+    public class UserAllRegisteredVehiclesViewComponent : ViewComponent
+    {
+        private readonly ICarsService carsService;
+        private readonly IUsersService usersService;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public UserAllRegisteredVehiclesViewComponent(
+            ICarsService carsService,
+            UserManager<ApplicationUser> userManager, IUsersService usersService)
+        {
+            this.carsService = carsService;
+            this.userManager = userManager;
+            this.usersService = usersService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string username, int page)
+        {
+            var allUserCars = this.carsService.GetAllUserCarsByUserUsername<CarViewModel>(username);
+            var user = this.usersService.GetUserByUsername<ApplicationUserViewModel>(username);
+
+            CarListViewModel model = new CarListViewModel
+            {
+                UserId = user.Id,
+                UserUsername = username,
+                Cars = allUserCars,
+            };
+
+            return this.View(model);
+        }
+    }
+}
---
+++ ASP.NET Core/Web/Tompany.Web/ViewComponents/Users/UserAllTripsViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tompany.Data.Models;
+using Tompany.Services.Data.Contracts;
+using Tompany.Web.ViewModels.Trips.ViewModels;
+
+namespace Tompany.Web.ViewComponents.Users
+{
+    public class UserAllTripsViewComponent : ViewComponent
+    {
+        private readonly ITripsService tripsService;
+
+        public UserAllTripsViewComponent(ITripsService tripsService)
+        {
+            this.tripsService = tripsService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string username, int page)
+        {
+            var allUserTrips = this.tripsService.GetUserTripsWithUsername<TripDetailsViewModel>(username);
+
+            TripListViewModel model = new TripListViewModel
+            {
+                Trips = allUserTrips,
+            };
+
+            return this.View(model);
+        }
+    }
+}
