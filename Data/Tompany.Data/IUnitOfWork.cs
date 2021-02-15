a6c9fa1e9fd9f8e3293f6e9d8344dcadec50baae
mvarbanov97 <momchil.varbanov97@gmail.com>
2021-02-15 17:19:03 +0200

    Introduced UnitOfWork

---
+++ Data/Tompany.Data/IUnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Tompany.Data.Common.Repositories;
+using Tompany.Data.Models;
+
+namespace Tompany.Data
+{
+    public interface IUnitOfWork
+    {
+        IDeletableEntityRepository<Car> Cars { get; }
+
+        IDeletableEntityRepository<ApplicationUser> Users { get; }
+
+        IDeletableEntityRepository<Group> Groups { get; }
+
+        IDeletableEntityRepository<ChatMessage> ChatMessages { get; }
+
+        IRepository<Destination> Destinations { get; }
+
+        IRepository<Trip> Trips { get; }
+
+        IRepository<TripRequest> TripRequests { get; }
+
+        IRepository<UserTrip> UserTrips { get; }
+
+        IRepository<UserRating> UserRatings { get; }
+        Task CompleteAsync();
+    }
+}
---
+++ Data/Tompany.Data/UnitOfWork.cs
@@ -0,0 +1,50 @@
+﻿namespace Tompany.Data
+{
+    using System.Threading.Tasks;
+
+    using Tompany.Data.Common.Repositories;
+    using Tompany.Data.Models;
+    using Tompany.Data.Repositories;
+
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext context;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            this.context = context;
+            this.Cars = new EfDeletableEntityRepository<Car>(context);
+            this.Users = new EfDeletableEntityRepository<ApplicationUser>(context);
+            this.Groups = new EfDeletableEntityRepository<Group>(context);
+            this.ChatMessages = new EfDeletableEntityRepository<ChatMessage>(context);
+            this.Destinations = new EfRepository<Destination>(context);
+            this.Trips = new EfRepository<Trip>(context);
+            this.TripRequests = new EfRepository<TripRequest>(context);
+            this.UserTrips = new EfRepository<UserTrip>(context);
+            this.UserRatings = new EfRepository<UserRating>(context);
+        }
+
+        public IDeletableEntityRepository<Car> Cars { get; private set; }
+
+        public IDeletableEntityRepository<ApplicationUser> Users { get; private set; }
+
+        public IDeletableEntityRepository<Group> Groups { get; private set; }
+
+        public IDeletableEntityRepository<ChatMessage> ChatMessages { get; private set; }
+
+        public IRepository<Destination> Destinations { get; private set; }
+
+        public IRepository<Trip> Trips { get; private set; }
+
+        public IRepository<TripRequest> TripRequests { get; private set; }
+
+        public IRepository<UserTrip> UserTrips { get; private set; }
+
+        public IRepository<UserRating> UserRatings { get; private set; }
+
+        public async Task CompleteAsync()
+        {
+            await this.context.SaveChangesAsync();
+        }
+    }
+}
