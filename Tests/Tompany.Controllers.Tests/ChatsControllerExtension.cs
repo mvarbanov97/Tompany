02f025907e2b9b6303a582901ffef6a5ed6ca4f8
mvarbanov97 <momchil.varbanov97@gmail.com>
2021-02-23 19:07:23 +0200

    Moved Test Controller project into the Test folder

---
+++ Tests/Tompany.Controllers.Tests/ChatsControllerExtension.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Tompany.Data;
+using Tompany.Data.Common.Repositories;
+using Tompany.Data.Models;
+using Tompany.Web.Infrastructure;
+
+namespace Tompany.Controllers.Tests
+{
+    public static class ChatsControllerExtension
+    {
+        public static Mock<UserManager<ApplicationUser>> MockUserManager()
+        {
+            var currentUser = new ApplicationUser { Id = "1", UserName = "testUsername" };
+
+            var mockUserManager = new Mock<UserManager<ApplicationUser>>(
+                    new Mock<IUserStore<ApplicationUser>>().Object,
+                    new Mock<IOptions<IdentityOptions>>().Object,
+                    new Mock<IPasswordHasher<ApplicationUser>>().Object,
+                    new IUserValidator<ApplicationUser>[0],
+                    new IPasswordValidator<ApplicationUser>[0],
+                    new Mock<ILookupNormalizer>().Object,
+                    new Mock<IdentityErrorDescriber>().Object,
+                    new Mock<IServiceProvider>().Object,
+                    new Mock<ILogger<UserManager<ApplicationUser>>>().Object);
+            mockUserManager.Setup(x => x.GetUserAsync(It.IsAny<ClaimsPrincipal>()))
+                 .ReturnsAsync(currentUser);
+
+            return mockUserManager;
+        }
+
+        public static Mock<IChatService> MockChatService()
+        {
+            var mockChatService = new Mock<IChatService>();
+
+            mockChatService.Setup(x => x.ExtractAllMessages(It.IsAny<string>()))
+                .ReturnsAsync(new List<ChatMessage>
+                {
+                    new ChatMessage
+                    {
+                        Content = "testContent"
+                    },
+                    new ChatMessage
+                    {
+                        Content = "testContent2"
+                    },
+                });
+
+            return mockChatService;
+        }
+
+        public static Mock<IUnitOfWork> MockUnitOfWork()
+        {
+            var mockUserRepository = new Mock<IDeletableEntityRepository<ApplicationUser>>();
+
+            var testTrip = new Trip() { Id = "1" };
+            var listTrip = new List<Trip>();
+            listTrip.Add(testTrip);
+
+            var mockTripRepository = new Mock<IRepository<Trip>>();
+            mockTripRepository.Setup(x => x.All()).Returns(listTrip.AsQueryable());
+
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockUnitOfWork.Setup(x => x.Trips.All()).Returns(It.IsAny<IQueryable<Trip>>());
+            mockUnitOfWork.Setup(u => u.Users).Returns(mockUserRepository.Object).Verifiable();
+            mockUnitOfWork.Setup(u => u.Trips).Returns(mockTripRepository.Object).Verifiable(); // first trip with given ID
+
+            var testReturn = mockUnitOfWork.Object.Trips.All();
+
+            return mockUnitOfWork;
+        }
+
+    }
+}
---
+++ Tests/Tompany.Controllers.Tests/ChatsControllerTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Tompany.Data;
+using Tompany.Data.Models;
+using Tompany.Services.Data.Contracts;
+using Tompany.Web.Controllers;
+using Tompany.Web.Infrastructure;
+using Tompany.Web.ViewModels.Chat;
+using Xunit;
+
+namespace Tompany.Controllers.Tests
+{
+    public class ChatsControllerTests
+    {
+        private ChatsController sut;
+        private Mock<UserManager<ApplicationUser>> userManager;
+        private Mock<IChatService> mockChatService;
+        private Mock<IUnitOfWork> mockUnitOfWork;
+
+        public ChatsControllerTests()
+        {
+            this.userManager = ChatsControllerExtension.MockUserManager();
+            this.mockChatService = ChatsControllerExtension.MockChatService();
+            this.mockUnitOfWork = ChatsControllerExtension.MockUnitOfWork();
+
+            this.sut = new ChatsController(userManager.Object, mockChatService.Object, mockUnitOfWork.Object);
+        }
+
+        [Fact]
+        public async Task GetIndexShouldReturnViewResultSuccessfully()
+        {
+            //Act
+            var result = await this.sut.Index("testUsername", "testGroup");
+
+            //Assert
+            ViewResult viewResult = Assert.IsType<ViewResult>(result);
+            Assert.IsType<ChatViewModel>(viewResult.Model);
+        }
+
+        [Fact]
+        public async Task GetGroupChatShouldReturnViewResultSuccessfully()
+        {
+            //Arrange
+
+
+            //Act
+            var result = await this.sut.GroupChat("testGroup", "1");
+
+            //Assert
+            ViewResult viewResult = Assert.IsType<ViewResult>(result);
+            Assert.IsType<GroupChatViewModel>(viewResult.Model);
+        }
+
+
+
+    }
+}
