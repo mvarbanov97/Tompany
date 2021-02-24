5f7ea7898601e1e2a88418fdda8a14866ac3b8e2
mvarbanov97 <momchil.varbanov97@gmail.com>
2021-02-24 12:53:52 +0200

    Added CountryCode and ZipCode domain entities

---
+++ Data/Tompany.Data.Models/CountryCode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Tompany.Data.Common.Models;
+
+namespace Tompany.Data.Models
+{
+    public class CountryCode : BaseModel<string>, IDeletableEntity
+    {
+        public CountryCode()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        [Required]
+        [MaxLength(10)]
+        public string Code { get; set; }
+
+        public ICollection<Country> Coutries { get; set; } = new HashSet<Country>();
+
+        public ICollection<ApplicationUser> ApplicationUsers { get; set; } = new HashSet<ApplicationUser>();
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+    }
+}
---
+++ Data/Tompany.Data.Models/ZipCode.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using Tompany.Data.Common.Models;
+
+namespace Tompany.Data.Models
+{
+    public class ZipCode : BaseModel<string>, IDeletableEntity
+    {
+        public ZipCode()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        [Required]
+        public int Code { get; set; }
+
+        // [Required]
+        [ForeignKey(nameof(City))]
+        public string CityId { get; set; }
+
+        public City City { get; set; }
+
+        public ICollection<ApplicationUser> ApplicationUsers { get; set; } = new HashSet<ApplicationUser>();
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+    }
+}
